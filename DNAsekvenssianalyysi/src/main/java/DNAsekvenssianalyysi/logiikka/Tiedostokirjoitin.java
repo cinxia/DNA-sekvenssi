a6c38842fb76d03a4bef4e33f4f9e81d5453fd88
cinxia <malla.kirjokangas@helsinki.fi>
2015-05-26 18:09:29 +0300

    tiedostokirjoittimen ja testiluokan muokkaminen

--- DNAsekvenssianalyysi/src/main/java/DNAsekvenssianalyysi/logiikka/Tiedostokirjoitin.java
+++ DNAsekvenssianalyysi/src/main/java/DNAsekvenssianalyysi/logiikka/Tiedostokirjoitin.java
@@ -5,145 +5,146 @@ import java.io.*;
 public class Tiedostokirjoitin {
 
     private PrintWriter kirjoitin;
-    
-    // konstruktori vaatii vielä korjaamista
+    private boolean luonninOnnistuminen;
+
     public Tiedostokirjoitin(String tiedostopolku) {
-        alustaKirjoitin(tiedostopolku);
+        luonninOnnistuminen = luoKirjoitin(tiedostopolku);
 
     }
-    
-    private boolean alustaKirjoitin(String tiedostopolku){
-        try{
-        File raportti = new File(tiedostopolku);
-        FileWriter fw = new FileWriter(raportti, true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        this.kirjoitin = new PrintWriter(bw);
-        }
-        catch (Exception e){
-            return false;
-        }
-        return true;
-    }
 
-    public boolean kirjoitaSyoteSekvenssi(String sekvenssi) {
+    private boolean luoKirjoitin(String tiedostopolku) {
         try {
-            this.kirjoitin.append("Syötetty DNA-sekvenssi:\n");
-            this.kirjoitin.append(sekvenssi);
+            File raportti = new File(tiedostopolku);
+            FileWriter fw = new FileWriter(raportti, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            this.kirjoitin = new PrintWriter(bw);
         } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaSekvenssinPituus(int pituus){
-        try{
-            this.kirjoitin.append("DNA-sekvenssin pituus: "+ pituus);
-        }
-        catch(Exception e){
+
+    public boolean getLuonninOnnistuminen() {
+        return this.luonninOnnistuminen;
+    }
+
+    public boolean kirjoitaSyoteSekvenssi(String sekvenssi) {
+        try {
+            this.kirjoitin.append("Syötetty DNA-sekvenssi:" + System.getProperty("line.separator"));
+            this.kirjoitin.append(sekvenssi + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
+
+    public boolean kirjoitaSekvenssinPituus(int pituus) {
+        try {
+            this.kirjoitin.append("DNA-sekvenssin pituus: " + pituus + System.getProperty("line.separator"));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
     public boolean kirjoitaEmasFrekvenssit(int frekvenssiA, int frekvenssiT,
-            int frekvenssiC, int frekvenssiG){
-        try{
-            this.kirjoitin.append("Emäsfrekvenssit:\n");
-            this.kirjoitin.append("A: "+ frekvenssiA);
-            this.kirjoitin.append("T: " + frekvenssiT);
-            this.kirjoitin.append("C: " + frekvenssiC);
-            this.kirjoitin.append("G: "+ frekvenssiG);
-        }
-        catch(Exception e){
+            int frekvenssiC, int frekvenssiG) {
+        try {
+            this.kirjoitin.append("Emäsfrekvenssit:" + System.getProperty("line.separator"));
+            this.kirjoitin.append("A: " + frekvenssiA + System.getProperty("line.separator"));
+            this.kirjoitin.append("T: " + frekvenssiT + System.getProperty("line.separator"));
+            this.kirjoitin.append("C: " + frekvenssiC + System.getProperty("line.separator"));
+            this.kirjoitin.append("G: " + frekvenssiG + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaGCOsuus(double osuus){
-        try{
-            this.kirjoitin.append("GC-osuus: "+ osuus);
-        }
-        catch(Exception e){
+
+    public boolean kirjoitaGCOsuus(double osuus) {
+        try {
+            this.kirjoitin.append("GC-osuus: " + osuus + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaPyrimidiiniPuriiniSuhde(String suhdeluku){
-        try{
+
+    public boolean kirjoitaPyrimidiiniPuriiniSuhde(String suhdeluku) {
+        try {
             this.kirjoitin.append("Pyrimidiini-puriinisuhde: ");
-            this.kirjoitin.append(suhdeluku);
-        }
-        catch(Exception e){
+            this.kirjoitin.append(suhdeluku + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaVastinjuoste(String vastinjuoste){
-        try{
-            this.kirjoitin.append("Syötetyn DNA-sekvenssin vastinjuoste:\n");
-            this.kirjoitin.append(vastinjuoste);
-        }
-        catch(Exception e){
+
+    public boolean kirjoitaVastinjuoste(String vastinjuoste) {
+        try {
+            this.kirjoitin.append("Syötetyn DNA-sekvenssin vastinjuoste:"
+                    + System.getProperty("line.separator"));
+            this.kirjoitin.append(vastinjuoste + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaLahettiRna(String lahettiRna){
-        try{
-            this.kirjoitin.append("Syötettyä DNA-sekvenssiä vastaava lähetti-RNA\n");
-        }
-        catch(Exception e){
+
+    public boolean kirjoitaLahettiRna(String lahettiRna) {
+        try {
+            this.kirjoitin.append("Syötettyä DNA-sekvenssiä vastaava lähetti-RNA:"
+                    + System.getProperty("line.separator"));
+            this.kirjoitin.append(lahettiRna + System.getProperty("line.separator"));
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean kirjoitaJaollisuusviesti(String viesti){
-        try{
-            switch(viesti){
-                case("Tasan"):
+
+    public boolean kirjoitaJaollisuusviesti(String viesti) {
+        try {
+            switch (viesti) {
+                case ("Tasan"):
                     this.kirjoitin.append("Lähetti-RNA on kolmella jaollinen. "
-                            + "Kaikki emäkset on muutettu aminohapoiksi.");
+                            + "Kaikki emäkset on muutettu aminohapoiksi."
+                            + System.getProperty("line.separator"));
                     break;
-                case("Yksi liikaa"):
+                case ("Yksi liikaa"):
                     this.kirjoitin.append("Lähetti-RNA ei ole kolmella jaollinen. "
-                            + "Viimeinen emäs on jätetty pois.");
+                            + "Viimeinen emäs on jätetty pois."
+                            + System.getProperty("line.separator"));
                     break;
-                case("Kaksi liikaa"):
+                case ("Kaksi liikaa"):
                     this.kirjoitin.append("Lähetti-RNA ei ole kolmella jaollinen. "
-                            + "Kaksi viimeistä emästä on jätetty pois.");
+                            + "Kaksi viimeistä emästä on jätetty pois."
+                            + System.getProperty("line.separator"));
                     break;
-            }}
-            catch(Exception e){
-                    return false;
-                    }
-        return true;
-    }
-    
-    public boolean kirjoitaAminohappoketju(String ketju){
-        try{
-            this.kirjoitin.append("Lähetti-RNA:ta vastaava aminohappoketju:\n");
-            this.kirjoitin.append(ketju);
-        }
-        catch(Exception e){
+            }
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    public boolean suljeTiedosto(){
-        try{
+
+    public boolean kirjoitaAminohappoketju(String ketju) {
+        try {
+            this.kirjoitin.append("Lähetti-RNA:ta vastaava aminohappoketju:"
+                    + System.getProperty("line.separator"));
+            this.kirjoitin.append(ketju + System.getProperty("line.separator"));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean suljeTiedosto() {
+        try {
             this.kirjoitin.close();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
-    
+
 }
--- DNAsekvenssianalyysi/src/test/java/DNAsekvenssianalyysi/logiikka/TiedostokirjoitinTest.java
+++ DNAsekvenssianalyysi/src/test/java/DNAsekvenssianalyysi/logiikka/TiedostokirjoitinTest.java
@@ -1,4 +1,3 @@
-
 package DNAsekvenssianalyysi.logiikka;
 
 import org.junit.After;
@@ -8,75 +7,91 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class TiedostokirjoitinTest {
-    
+
     Tiedostokirjoitin testikirjoitin;
-    
+
     public TiedostokirjoitinTest() {
     }
-    
 
-    
     @Before
     public void setUp() {
         testikirjoitin = new Tiedostokirjoitin("F:\\testiraportti.txt");
     }
+
+    @Test
+    public void luoKirjoitinToimiiKunTiedostopolkuOikein() {
+        assertTrue(testikirjoitin.getLuonninOnnistuminen());
+    }
+
+    @Test
+    public void kirjoitaSyoteSekvenssiToimii() {
+        assertTrue(testikirjoitin.kirjoitaSyoteSekvenssi("AATTCCG"));
+    }
+
+    @Test
+    public void kirjoitaSekvenssinPituusToimii() {
+        assertTrue(testikirjoitin.kirjoitaSekvenssinPituus(15));
+    }
+
+    @Test
+    public void kirjoitaEmasfrekvenssitToimii() {
+        assertTrue(testikirjoitin.kirjoitaEmasFrekvenssit(12, 4, 1, 0));
+    }
+
+    @Test
+    public void kirjoitaGCOsuusToimii() {
+        assertTrue(testikirjoitin.kirjoitaGCOsuus(0.75));
+    }
+
+    @Test
+    public void kirjoitaPyrimidiinPuriiniSuhdeToimii() {
+        assertTrue(testikirjoitin.kirjoitaPyrimidiiniPuriiniSuhde("3:7"));
+    }
+
+    @Test
+    public void kirjoitaVastinjuosteToimii() {
+        assertTrue(testikirjoitin.kirjoitaVastinjuoste("TTAAGGC"));
+    }
+
+    @Test
+    public void kirjoitaLahettiRnaToimii() {
+        assertTrue(testikirjoitin.kirjoitaLahettiRna("AUAUACGC"));
+
+    }
+
+    @Test
+    public void kirjoitaJaollisuusviestiToimiiKunTasan() {
+        assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Tasan"));
+    }
+
+    @Test
+    public void kirjoitaJaollisuusviestiToimiiKunYksiLiikaa() {
+        assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Yksi liikaa"));
+    }
+
+    @Test
+    public void kirjoitaJaollisuusviestiToimiiKunKaksiLiikaa() {
+        assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Kaksi liikaa"));
+    }
+
+    @Test
+    public void kirjoitaAminohappoketjuToimii() {
+        assertTrue(testikirjoitin.kirjoitaAminohappoketju("MLT"));
+    }
     
-@Test
-public void kirjoitaSyoteSekvenssiToimii(){
-    assertTrue(testikirjoitin.kirjoitaSyoteSekvenssi("AATTCCG"));
-}
+    @Test
+    public void suljeTiedostoToimii(){
+        assertTrue(testikirjoitin.suljeTiedosto());
+    }
     
-@Test
-public void kirjoitaSekvenssinPituusToimii(){
-    assertTrue(testikirjoitin.kirjoitaSekvenssinPituus(15));
-}
-
-@Test
-public void kirjoitaEmasfrekvenssitToimii(){
-    assertTrue(testikirjoitin.kirjoitaEmasFrekvenssit(12, 4, 1, 0));
-}
-
-@Test
-public void kirjoitaGCOsuusToimii(){
-    assertTrue(testikirjoitin.kirjoitaGCOsuus(0.75));
-}
-
-@Test
-public void kirjoitaPyrimidiinPuriiniSuhdeToimii(){
-    assertTrue(testikirjoitin.kirjoitaPyrimidiiniPuriiniSuhde("3:7"));
-}
-
-@Test
-public void kirjoitaVastinjuosteToimii(){
-    assertTrue(testikirjoitin.kirjoitaVastinjuoste("TTAAGGC"));
-}
-
-@Test
-public void kirjoitaLahettiRnaToimii(){
-    assertTrue(testikirjoitin.kirjoitaLahettiRna("AUAUACGC"));
     
-}
-
-@Test
-public void kirjoitaJaollisuusviestiToimiiKunTasan(){
-    assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Tasan"));
-}
-
-@Test
-public void kirjoitaJaollisuusviestiToimiiKunYksiLiikaa(){
-    assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Yksi liikaa"));
-}
-
-@Test
-public void kirjoitaJaollisuusviestiToimiiKunKaksiLiikaa(){
-    assertTrue(testikirjoitin.kirjoitaJaollisuusviesti("Kaksi liikaa"));
-}
-
-@Test
-public void kirjoitaAminohappoketjuToimii(){
-    assertTrue(testikirjoitin.kirjoitaAminohappoketju("MLT"));
-}
+    @Test
+    public void kokoOperaatiotoimii(){
+        Tiedostokirjoitin kakkoskirjoitin = new Tiedostokirjoitin("F:\\testiraporttitoinen.txt");
+        kakkoskirjoitin.kirjoitaSyoteSekvenssi("ATCG");
+        kakkoskirjoitin.suljeTiedosto();
+    }
+    
 
 }
