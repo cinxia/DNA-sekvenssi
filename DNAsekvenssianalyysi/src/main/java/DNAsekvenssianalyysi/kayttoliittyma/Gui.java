160e3995d3b7369206c805300282788493291e7a
cinxia <malla.kirjokangas@helsinki.fi>
2015-06-06 10:23:48 +0300

    sijoittelumanageri

--- DNAsekvenssianalyysi/src/main/java/DNAsekvenssianalyysi/kayttoliittyma/Gui.java
+++ DNAsekvenssianalyysi/src/main/java/DNAsekvenssianalyysi/kayttoliittyma/Gui.java
@@ -14,21 +14,25 @@ public class Gui extends JFrame implements ActionListener{
     private ButtonGroup syotevalitsin;
     private JRadioButton tekstivalinta, tiedostovalinta;
     private JTextField tekstisyotekentta, tiedostosyotekentta, raporttitiedostokentta;
-    private JLabel syotevalinta, raporttitiedosto, analyysivalinta;
+    private JLabel syoteteksti, raporttiteksti, analyysiteksti;
     
     public Gui(){     
         JPanel paneeli = (JPanel)this.getContentPane();   
-        paneeli.setLayout(new FlowLayout());
+       // paneeli.setLayout(new FlowLayout());
+        GroupLayout layout = new GroupLayout(paneeli);
+        paneeli.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
         
         //ikkunan asetukset
         this.setTitle("DNA-sekvenssin analysointityökalu");
-        this.setSize(700, 500);
+        this.setSize(700, 600);
         this.setResizable(true);
-        this.setLocation(100, 100);
+        this.setLocation(100, 0);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        syotevalinta = new JLabel("Valitse mistä DNA-sekvenssi luetaan.");
+        syoteteksti = new JLabel("Valitse mistä DNA-sekvenssi luetaan.");
         
         syotevalitsin = new ButtonGroup();
         tekstivalinta = new JRadioButton("Lue tekstikentästä", false);
@@ -40,30 +44,57 @@ public class Gui extends JFrame implements ActionListener{
         tiedostosyotekentta = new JTextField("Luettava tiedosto", 20);
         lukunappi = new JButton("Selaa");
         
-        raporttitiedosto = new JLabel("Valise tiedosto, johon raportti kirjoitetaan.");
+        raporttiteksti = new JLabel("Valise tiedosto, johon raportti kirjoitetaan.");
         raporttitiedostokentta = new JTextField("Tallennettava tiedosto");
         kirjoitusnappi = new JButton("Selaa");
         
-        analyysivalinta = new JLabel("Valitse suoritettavat analyysit.");
+        analyysiteksti = new JLabel("Valitse suoritettavat analyysit.");
         analyysinappi = new JButton("Suorita analyysi");
         analyysinappi.addActionListener(this);
         
-        //komponenttien sijoittelu ikkunaan
-        paneeli.add(syotevalinta);
-        paneeli.add(tekstivalinta);
-        paneeli.add(tekstisyotekentta);
-        paneeli.add(tiedostovalinta);
-        paneeli.add(tiedostosyotekentta);
-        paneeli.add(lukunappi);
-        paneeli.add(raporttitiedosto);
-        paneeli.add(raporttitiedostokentta);
-        paneeli.add(kirjoitusnappi);
-        paneeli.add(analyysivalinta); 
+        //horisontaalinen järjestys
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(syoteteksti)
+                    .addComponent(tekstivalinta)
+                    .addComponent(tekstisyotekentta)
+                    .addComponent(tiedostovalinta)
+                    .addComponent(tiedostosyotekentta)
+                    .addComponent(raporttiteksti)
+                    .addComponent(raporttitiedostokentta)
+                    .addComponent(analyysiteksti) 
+                    // lisää checkboxit tähän
+                    .addComponent(analyysinappi))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(lukunappi)
+                    .addComponent(kirjoitusnappi))
+                
+        );
         
-        paneeli.add(analyysinappi);
+        // vertikaalinen järjestys
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(syoteteksti)
+                .addComponent(tekstivalinta)
+                .addComponent(tekstisyotekentta)
+                .addComponent(tiedostovalinta)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(tiedostosyotekentta)
+                    .addComponent(lukunappi))
+                .addComponent(raporttiteksti)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(raporttitiedostokentta)
+                    .addComponent(kirjoitusnappi))
+                .addComponent(analyysiteksti)
+                // checkboxit tähän
+                .addComponent(analyysinappi)
+        );
+        
         
     }
     
+    @Override
     public void actionPerformed(ActionEvent e){
         try{
             if (e.getSource()==analyysinappi){
